41184d084d7484941455c23eae7510aa02a22baa
Jerruz <jw.buzd@gmail.com>
2021-10-25 08:26:58 +0300

    Init commit

---
+++ sys.argv_ex.py
@@ -0,0 +1,10 @@
+# #-*- coding: UTF-8 -*-
+from sys import argv
+
+script, first, second, third = argv
+
+print ("Этот скрипт называпввпается: ", script)
+print ("Значение первой переменной: ", first)
+print ("Значение второй переменной: ", second)
+print ("Значение третьей переменной: ", third)
+
